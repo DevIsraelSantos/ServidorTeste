d1df06cedf6a0d0a4c61c1bd91e54a72bfba0a1e
DevIsraelSantos <dev.israelsantos@gmail.com>
2020-11-29 20:37:50 -0300

    adicionado controle de erro

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
 const express = require('express');
 const app = express(); //instancia do express
+const morgan= require('morgan');
+
 
 const rotaProdutos = require('./routes/produtos');
 const rotaPedidos = require('./routes/pedidos');
 
+app.use(morgan('dev'));
 app.use('/produtos', rotaProdutos);
 app.use('/pedidos', rotaPedidos);
 
-app.use('/test', (req, res, next) => {    //(req = requisição, res = resposta, next = quando quer chamar outro metodo)
-    res.status(200).send({
-        mensagem: 'ok, deu certo'
-    });
+app.use((req, res, next) => {    //sem rota valida
+    const erro = new Error('Rota não encontrada');
+    erro.status = 404;
+    next(erro);
+});
+
+app.use((error, req, res, next) =>{
+    res.status(error.status || 500);
+    return res.send({
+        erro: {
+            mensagem: error.message
+        }
+    })
 });
 
 module.exports = app; //exporta o app
